d0ee4d5c1f6247c3b2b4667b2c9e12a9ce7653dc
Chazensign <chazensign@gmail.com>
2020-05-20 13:44:37 -0600

    restructured js functions, added new promise to fix race condition

--- components/FileFetcher.js
+++ components/FileFetcher.js
@@ -5,46 +5,64 @@ import RNFetchBlob from 'rn-fetch-blob';
 
 class FileFetcher extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       fileNames: [],
-      files: []
-    }
+      files: [],
+      // file object {
+      //              name: string,
+      //              path: cacheFilePathString,
+      //              type: fileExtesionString
+      //               }
+    };
   }
 
   componentDidMount() {
+    this.getFileNames()
+  }
+
+  getFileNames = () => {
     fetch('http://10.0.2.2:4040/files')
       .then(async res => {
-        let response = await res.json()
+        let response = await res.json();
         this.setState({fileNames: response.data})
         this.downloadFiles(response.data)
       })
       .catch(err => console.log(err))
   }
-  downloadFiles = async list => {
-    let filesArr = []
-    for (let i = 0; i < list.length; i++) {
-      RNFetchBlob.config({ fileCache: true })
-        .fetch('GET', `http://10.0.2.2:4040/file/${list[i]}`)
-        .then(res => {
-          console.log(res.path())
-          filesArr.push({name: list[i], path: res.path(), type: list[i].split('.')[1]})
-          if (i === list.length -1) this.setState({ files: filesArr });
-        });
-    }
-  }
 
-  
+  downloadFiles = async list => {
+    let filesArr = [];
+    for (let i = 0; i < list.length; i++) {
+      let fileInfo = await this.fetchFileCreatePath(list[i])
+      filesArr.push(fileInfo)
+      if (i === list.length - 1) this.setState({files: filesArr})
+    }
+  };
+
+  fetchFileCreatePath = nameStr => {
+    return new Promise((resolve, reject) => {
+      RNFetchBlob.config({fileCache: true})
+        .fetch('GET', `http://10.0.2.2:4040/file/${nameStr}`)
+        .then(res => {
+          resolve({
+            name: nameStr,
+            path: res.path(),
+            type: nameStr.split('.')[1],
+          });
+        });
+    });
+  };
 
   render() {
-    console.log(this.state)
+
     return (
       <>
-        {this.state.fileNames.map((fileName, i) => {
-          return <Text key={i}>{fileName}</Text>
+        {this.state.files.map((file, i) => {
+          return <Text key={i}>{file.name}</Text>;
         })}
       </>
-    )
+    );
   }
 }
 
